ae73519867deb8d552b4d178456e100b5faef446
Lisa Gee <zhusufeng@gmail.com>
2018-03-10 16:24:04 -0800

    Use moment to show date in a human-readable format in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder } from '../actions'; // this is passed in to props
+import moment from 'moment';
 
 class App extends Component {
   constructor (props) {
@@ -33,7 +34,7 @@ class App extends Component {
               <li key={reminder.id} className="list-group-item">
                 <div className="list-item">
                   <div>{reminder.text}</div>
-                  <div><em>{reminder.dueDate}</em></div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
                 </div>
                 <div 
                   className="list-item delete-button"
