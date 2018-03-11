b7481f79c75abf208fe1e8ca938438d27ce9918a
Lisa Gee <zhusufeng@gmail.com>
2018-03-10 16:37:12 -0800

    Set state to reminder cookie, update reminder cookie with ADD or DELETE

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 // helper func for adding reminder
 const reminder = (action) => {
@@ -21,13 +22,16 @@ const removeById = (state = [], id) => {
 // reducer
 const reminders = (state = [], action) => {
   let reminders = null; // proxy for state right now
+  state = read_cookie('reminders');
   switch (action.type) {
     case ADD_REMINDER:
       reminders = [...state, reminder(action)]; // spread old state into new array & add another element
       console.log('reminders as state', reminders);
+      bake_cookie('reminders', reminders); // add/update reminder cookie in browser
       return reminders;
     case DELETE_REMINDER:
       reminders = removeById(state, action.id);
+      bake_cookie('reminders', reminders); // update reminder cookie in browser
       return reminders;
     default:
       return state;
